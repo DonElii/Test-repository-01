ff9b768b8e2413fef20227b37b122b6ee9728fc7
ELI\Eli <gamerxa@gmail.com>
2016-09-07 11:09:11 +0000

    Bamboo trigger beallitva 3 percenkenti polling-ra, ezert tesztelunk

--- MainClass.java
+++ MainClass.java
@@ -3,7 +3,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Dog's World!");
+System.out.println("Hello Cat's World!");
 	}
 
 }
